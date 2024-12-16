73060a3d6907fc624d61f9ba4294205263d13b9e
alinaaksanova <aksyanova.alina@gmail.com>
2024-12-16 17:08:42 +0300

    изменения

--- lab7/task4/src/task4.py
+++ lab7/task4/src/task4.py
@@ -12,16 +12,16 @@ PATH_OUTPUT = os.path.join('..', '..', 'files', 'output.txt')
 def longest_common_subsequence(n, a, m, b):
     n = len(a)
     m = len(b)
-    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    l = [[0] * (m + 1) for _ in range(n + 1)]
 
     for i in range(1, n + 1):
         for j in range(1, m + 1):
             if a[i - 1] == b[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
+                l[i][j] = l[i - 1][j - 1] + 1
             else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                l[i][j] = max(l[i - 1][j], l[i][j - 1])
 
-    return dp[n][m]
+    return l[n][m]
 
 def task1():
     n, a, m, b = inp(PATH_INPUT, 0, 'task4')
--- lab7/task5/src/task5.py
+++ lab7/task5/src/task5.py
@@ -11,17 +11,17 @@ PATH_OUTPUT = os.path.join('..', '..', 'files', 'output.txt')
 
 def subsequence(n, a, m, b, l, c):
 
-    dp = [[[0] * (l + 1) for _ in range(m + 1)] for __ in range(n + 1)]
+    posl = [[[0] * (l + 1) for _ in range(m + 1)] for __ in range(n + 1)]
 
     for i in range(1, n + 1):
         for j in range(1, m + 1):
             for k in range(1, l + 1):
                 if a[i - 1] == b[j - 1] == c[k - 1]:
-                    dp[i][j][k] = dp[i - 1][j - 1][k - 1] + 1
+                    posl[i][j][k] = posl[i - 1][j - 1][k - 1] + 1
                 else:
-                    dp[i][j][k] = max(dp[i - 1][j][k], dp[i][j - 1][k], dp[i][j][k - 1])
+                    posl[i][j][k] = max(posl[i - 1][j][k], posl[i][j - 1][k], posl[i][j][k - 1])
 
-    return dp[n][m][l]
+    return posl[n][m][l]
 
 def task1():
     n, a, m, b, l, c = inp(PATH_INPUT, 0, 'task5')
